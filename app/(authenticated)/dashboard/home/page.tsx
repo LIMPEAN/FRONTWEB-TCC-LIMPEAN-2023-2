dd4e15e19abafa4f73071271959779cabfb0162d
Vinicius Nunes <viniciusnunes137@gmail.com>
2023-10-09 02:46:23 -0300

    feat: put biography

--- app/(authenticated)/dashboard/home/page.tsx
+++ app/(authenticated)/dashboard/home/page.tsx
@@ -8,10 +8,10 @@ import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './service/firebase';
 import { putApi } from './service/fetchApi';
 import { Tabs } from 'flowbite-react';
-import { HiUserCircle } from 'react-icons/hi';
-import { Label, TextInput, Select } from 'flowbite-react';
+import { Label, TextInput, Select, Textarea } from 'flowbite-react';
 import InputMask from 'react-input-mask';
 import { SHA256 } from 'crypto-js';
+import { log } from 'console';
 
 interface User {
   statusClient: string;
@@ -43,12 +43,12 @@ interface UserUpdateData {
   idGender?: number;
   name?: string;
   password?: string;
-  phones?: {
+  phones?: [{
     ddd?: string;
     phone?: string;
     newDDD?: string;
     newPhone?: string;
-  }[];
+  }];
   photoUser?: string;
 }
 
@@ -66,6 +66,7 @@ export default function HomeDash() {
   const [password1, setpassword1] = useState("");
   const [password2, setpassword2] = useState("");
   const [telefone, setTelefone] = useState("");
+  const [biografiaState, setbiografiaState] = useState("");
 
 
   let token: string | null = null;
@@ -135,6 +136,7 @@ export default function HomeDash() {
       })
       .then((result) => {
         setData(result.data);
+        setbiografiaState(result.data.biography)
         setIsLoading(false);
       })
       .catch((error) => {
@@ -159,23 +161,65 @@ export default function HomeDash() {
 
 
   const handleSubmit = (event: any) => {
-    event.preventDefault(); // Impede o comportamento padrão de envio do formulário
-
-    // Obtém os valores dos campos do formulário
+    event.preventDefault();
     const nome: string = event.target.nome.value;
     const genero: number = Number(event.target.genero.value);
     const senha1: string = event.target.senha1.value;
     const senha2: string = event.target.senha2.value;
-    // Verifica se as senhas coincidem
+
     if (senha1 !== senha2) {
       toast.error("As senhas não coincidem")
       event.target.reset();
       return;
     }
-
-
-    
     updateUser({ name: nome, idGender: genero, password: senha1 })
+
+    event.target.reset();
+  };
+
+  const handleBiographySubmit = (event: any) => {
+    event.preventDefault();
+    const biografia: string = event.target.biografia.value;
+
+    updateUser({ biography: biografia })
+
+    event.target.reset();
+  };
+
+  const handlePhoneSubmit = (event: any) => {
+
+    event.preventDefault();
+
+
+    const phone: string = event.target.telefone.value;
+
+    if (!phone) {
+      toast.error("Telefone inválido")
+      event.target.reset();
+      return;
+    }
+
+
+    const telefoneLimpo = phone.replace(/\D/g, '');
+    if (telefoneLimpo.length < 11) {
+      toast.error("Quantidade de dígitos insuficientes")
+      event.target.reset();
+      return;
+    }
+    const ddd = telefoneLimpo.slice(0, 2);
+    const restante = telefoneLimpo.slice(2);
+
+    const telefonesJson = {
+      ddd: data?.phone[0].ddd,
+      phone: data?.phone[0].numero,
+      newDDD: ddd,
+      newPhone: restante
+    }
+
+    // console.log(telefonesJson);
+
+
+    updateUser({ phones: [telefonesJson] })
 
     event.target.reset();
   };
@@ -186,12 +230,13 @@ export default function HomeDash() {
     idGender,
     name,
     password,
-    phones,
     photoUser,
+    phones
   }: UserUpdateData = {}) => {
 
 
 
+    // console.log(phones?.length == null ? true : false);
 
     const jsonUsuario = {
       "name": name ? name : null,
@@ -201,16 +246,15 @@ export default function HomeDash() {
       "photoUser": photoUser ? photoUser : null,
       "phones": [
         {
-          "ddd": null,
-          "phone": null,
-          "newDDD": null,
-          "newPhone": null
+          "ddd": phones?.length == null ? null : phones[0].ddd,
+          "phone": phones?.length == null ? null : phones[0].phone,
+          "newDDD": phones?.length == null ? null: phones[0].newDDD,
+          "newPhone": phones?.length == null ? null : phones[0].newPhone
         }
       ]
     }
 
-    console.log(jsonUsuario);
-    console.log(token);
+
 
     try {
       const response = await putApi(jsonUsuario, `http://${process.env.HOST}:8080/v1/limpean/client/${token}`, token!!);
@@ -226,7 +270,7 @@ export default function HomeDash() {
   }
 
   return (
-    <div className='flex flex-col gap-2 h-full overflow-auto pb-24'>
+    <div className='flex flex-col h-full overflow-auto'>
       <Breadcrumb aria-label="Default breadcrumb example">
         <Breadcrumb.Item
           href="#"
@@ -254,104 +298,123 @@ export default function HomeDash() {
         >
 
           <span className='text-2xl'>Informações Pessoais</span>
-          <div className='flex flex-col lg:flex-row gap-4 lg:grid-cols-2 w-full mt-4  overflow-y-scroll'>
-            <div className="flex flex-col 2xl:flex-row px-4 py-8 gap-4 items-center lg:w-3/5 h-fit text-gray-800  rounded-lg shadow dark:bg-gray-800 dark:text-white dark:border-gray-700 ">
-              <Image className='w-32 h-32 object-cover rounded-md' src={data?.photoUser ? data?.photoUser : "https://firebasestorage.googleapis.com/v0/b/tcc-limpean.appspot.com/o/imagens%2Fprofile-default.webp?alt=media&token=8a68000c-eb45-4948-9fae-f01a00a10d1e&_gl=1*1u1domm*_ga*MTAyMTA0OTYwOS4xNjk0NTU2NDQx*_ga_CW55HF8NVT*MTY5NjExNzIyOC4zLjEuMTY5NjExNzI4Ny4xLjAuMA.."} alt={"foto de perfil de " + data?.name} width={300} height={300} />
-              <div className='flex flex-col justify-between h-32 w-full items-center'>
-                <span className='font-bold capitalize text-lg'>{data?.name}</span>
-                <span className='capitalize text-gray-400'>{data?.gender}</span>
-                <div className='flex flex-col w-full'>
-                  <label htmlFor="fileInput" className="h-12 bg-blue-700 text-white flex text-center justify-center items-center custom-file-label font-medium rounded-lg">Troque a foto</label>
-                  <input id='fileInput' className='flex-col hidden' type='file' onChange={handleUpload} accept=".png, .jpg, .jpeg, .avif, .gif"
-                  />
+          <div className='flex flex-col gap-4'>
+            <div className='flex flex-col lg:flex-row gap-4  w-full mt-4  overflow-y-scroll'>
+              <div className="flex flex-col 2xl:flex-row px-4  py-8 gap-4 items-center lg:w-3/5 text-gray-800  rounded-lg shadow dark:bg-gray-800 dark:text-white dark:border-gray-700 ">
+                <Image className='w-32 h-32 object-cover rounded-md' src={data?.photoUser ? data?.photoUser : "https://firebasestorage.googleapis.com/v0/b/tcc-limpean.appspot.com/o/imagens%2Fprofile-default.webp?alt=media&token=8a68000c-eb45-4948-9fae-f01a00a10d1e&_gl=1*1u1domm*_ga*MTAyMTA0OTYwOS4xNjk0NTU2NDQx*_ga_CW55HF8NVT*MTY5NjExNzIyOC4zLjEuMTY5NjExNzI4Ny4xLjAuMA.."} alt={"foto de perfil de " + data?.name} width={300} height={300} />
+                <div className='flex flex-col justify-between h-32 w-full items-center'>
+                  <span className='font-bold capitalize text-lg'>{data?.name}</span>
+                  <span className='capitalize text-gray-400'>{data?.gender}</span>
+                  <div className='flex flex-col w-full'>
+                    <label htmlFor="fileInput" className="h-12 bg-blue-700 text-white flex text-center justify-center items-center custom-file-label font-medium rounded-lg">Troque a foto</label>
+                    <input id='fileInput' className='flex-col hidden' type='file' onChange={handleUpload} accept=".png, .jpg, .jpeg, .avif, .gif"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className='flex flex-col gap-4 2xl:flex-row w-full'>
-              <form onSubmit={handleSubmit} className='flex flex-col p-4 gap-4 items-start w-full text-gray-800  rounded-lg shadow dark:bg-gray-800 dark:text-white dark:border-gray-700'>
-                <div className='flex w-full gap-4'>
-                  <TextInput
-                    type="text"
-                    className='w-full'
-                    id="nome"
-                    name="nome"
-                    placeholder={data?.name}
-                    value={nome}
-                    onChange={(e) => setNome(e.target.value)}
-                  />
-                  <Select
-                    className='w-full'
-                    id="genero"
-                    name="genero"
-                    onChange={(e) => setidGender(e.target.value)}
-                  >
-                    <option value="1" >Feminino</option>
-                    <option value="2" selected>Masculino</option>
-                    <option value="3">Outros</option>
-                  </Select>
-                </div>
-                <div className='flex w-full gap-4'>
-                  <TextInput
-                    type='password'
-                    className='w-full'
-                    id="senha1"
-                    name="senha1"
-                    placeholder="*********"
-                    onChange={(e) => setpassword1(e.target.value)}
-                  />
-                  <TextInput
-                    type='password'
-                    className='w-full'
-                    id="senha2"
-                    name="senha2"
-                    placeholder="*********"
-                    onChange={(e) => setpassword2(e.target.value)}
-                  />
-                </div>
-                <div className='flex w-full gap-4'>
-                  <button
-                    type='submit'
-                    className='h-12 w-full bg-blue-700 text-white flex text-center justify-center items-center custom-file-label font-medium rounded-lg'
-                  >
-                    Enviar
-                  </button>
-                </div>
-              </form>
+              <div className='flex flex-col gap-4 2xl:flex-row w-full'>
+                <form onSubmit={handleSubmit} className='flex flex-col p-4 gap-4 items-start w-full text-gray-800  rounded-lg shadow dark:bg-gray-800 dark:text-white dark:border-gray-700'>
+                  <div className='flex w-full gap-4'>
+                    <TextInput
+                      type="text"
+                      className='w-full'
+                      id="nome"
+                      name="nome"
+                      placeholder={data?.name}
+                      value={nome}
+                      onChange={(e) => setNome(e.target.value)}
+                    />
+                    <Select
+                      className='w-full'
+                      id="genero"
+                      name="genero"
+                      onChange={(e) => setidGender(e.target.value)}
+                    >
+                      <option value="1" >Feminino</option>
+                      <option value="2" selected>Masculino</option>
+                      <option value="3">Outros</option>
+                    </Select>
+                  </div>
+                  <div className='flex w-full gap-4'>
+                    <TextInput
+                      type='password'
+                      className='w-full'
+                      id="senha1"
+                      name="senha1"
+                      placeholder="*********"
+                      onChange={(e) => setpassword1(e.target.value)}
+                    />
+                    <TextInput
+                      type='password'
+                      className='w-full'
+                      id="senha2"
+                      name="senha2"
+                      placeholder="*********"
+                      onChange={(e) => setpassword2(e.target.value)}
+                    />
+                  </div>
+                  <div className='flex w-full gap-4'>
+                    <button
+                      type='submit'
+                      className='h-12 w-full bg-blue-700 text-white flex text-center justify-center items-center custom-file-label font-medium rounded-lg'
+                    >
+                      Enviar
+                    </button>
+                  </div>
+                </form>
 
-              <div className='h-full flex flex-col justify-center lg:flex-row px-4 py-8 gap-4 items-center w-full text-gray-800  rounded-lg shadow dark:bg-gray-800 dark:text-white dark:border-gray-700'>
-                <div className='flex flex-col w-full'>
-                  <label htmlFor="numeroAntigo" title='Numero antigo'>Numero antigo</label>
-                  <TextInput id='numeroAntigo' type="text" className='w-full' placeholder={
-                    (data?.phone[0].ddd ? data?.phone[0].ddd : "11") +
-                    (data?.phone[0].numero ? data?.phone[0].numero : "99999999")
-                  } disabled />
-                </div>
-                <div className='flex flex-col w-full'>
-                  <label htmlFor="numeroNovo" title='Numero antigo'>Numero novo</label>
-                  <InputMask
-                    mask="(99) 99999-9999"
-                    maskPlaceholder={null}
-                    type="text"
-                    placeholder='Digite seu telefone aqui...'
-
-                    id="telefone"
-                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                  />
-                </div>
-                <div className='flex items-end justify-end h-full w-full lg:w-48'>
-                  <button
-                    type='submit'
-                    className='h-12 w-full bg-blue-700 text-white flex text-center justify-center items-center custom-file-label font-medium rounded-lg'
-                  >
-                    Enviar
-                  </button>
-                </div>
+                <form onSubmit={handlePhoneSubmit} className='h-full flex flex-col justify-center lg:flex-row px-4 py-8 gap-4 items-center w-full text-gray-800  rounded-lg shadow dark:bg-gray-800 dark:text-white dark:border-gray-700'>
+                  <div className='flex flex-col w-full'>
+                    <label htmlFor="numeroAntigo" title='Numero antigo'>Numero antigo</label>
+                    <TextInput id='numeroAntigo' type="text" className='w-full' placeholder={
+                      (data?.phone[0].ddd ? data?.phone[0].ddd : "11") +
+                      (data?.phone[0].numero ? data?.phone[0].numero : "99999999")
+                    } disabled />
+                  </div>
+                  <div className='flex flex-col w-full'>
+                    <label htmlFor="numeroNovo" title='Numero antigo'>Numero novo</label>
+                    <InputMask
+                      mask="(99) 99999-9999"
+                      maskPlaceholder={null}
+                      type="text"
+                      placeholder='Digite seu telefone aqui...'
+                      id="telefone"
+                      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                      onChange={(e) => setTelefone(e.target.value)}
+                    />
+                  </div>
+                  <div className='flex items-end justify-end h-full w-full lg:w-48'>
+                    <button
+                      type='submit'
+                      className='h-12 w-full bg-blue-700 text-white flex text-center justify-center items-center custom-file-label font-medium rounded-lg'
+                    >
+                      Enviar
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
-          </div >
+            <form onSubmit={handleBiographySubmit} className='h-full flex flex-col justify-center px-4 py-8 gap-4 w-full text-gray-800  rounded-lg shadow dark:bg-gray-800 dark:text-white dark:border-gray-700'>
+              <Label
+                htmlFor="biografia"
+                value="Biografia"
+              />
+              <Textarea
+                id="biografia"
+                placeholder="Leave a comment..."
+                value={biografiaState}
+                rows={4}
+                onChange={(e) => setbiografiaState(e.target.value)}
+              />
+              <button
+                type='submit'
+                className='h-12 w-full bg-blue-700 text-white flex text-center justify-center items-center custom-file-label font-medium rounded-lg'
+              >
+                Enviar
+              </button>
+            </form>
+          </div>
         </Tabs.Item>
-
-
         <Tabs.Item
           active
           title="Endereço"
