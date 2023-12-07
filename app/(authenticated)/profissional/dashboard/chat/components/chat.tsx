3af08f992b372ba74e3c6e22899fa4c94ebe8dff
Vinicius Nunes <viniciusnunes137@gmail.com>
2023-12-06 23:42:45 -0300

    Update chat.tsx

--- app/(authenticated)/profissional/dashboard/chat/components/chat.tsx
+++ app/(authenticated)/profissional/dashboard/chat/components/chat.tsx
@@ -156,7 +156,7 @@ export default function ChatComponent({ servico, onClose, id }: ChatComponentPro
     let horas = dataAtual.getHours().toString().padStart(2, '0');
     let minutos = dataAtual.getMinutes().toString().padStart(2, '0');
 
-    let dataFormatada = `${ano}-${mes}-${Number(dia) - 1}`;
+    let dataFormatada = `${ano}-${mes}-${dia}`;
 
     const json = {
       serviceMysqlId: Number(servico.serviceId),
